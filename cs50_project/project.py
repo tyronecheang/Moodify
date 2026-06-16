2d69c67f9a083f9ff0aa89118fc7bdc163089128
Tyrone Cheang <tcheang@my.bcit.ca>
2026-06-16 16:30:28 -0700

    Added get_weather function using open weather map api

--- cs50_project/project.py
+++ cs50_project/project.py
@@ -1,15 +1,34 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.getenv("OPEN_WEATHER_KEY")
+open_weather_key = os.getenv("OPEN_WEATHER_KEY")
 
 def main():
-    ...
+    weather = get_weather("Vancouver")
+
+    if weather is None:
+        print("City not found or API error")
+        return
 
 def get_weather(location):
-    ...
+    url = "https://api.openweathermap.org/data/2.5/weather"
+
+    params = {
+        "q": location,
+        "appid": open_weather_key,
+        "units": "metric"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if response.status_code != 200:
+        return None
+    
+    return data["weather"][0]["main"]
 
 def weather_to_bpm(weather):
     ...
